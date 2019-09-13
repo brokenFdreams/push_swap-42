66c92ceff665e8ea3387d81a089412d927b21b41
brokenFdreams <brokenFdreams@protonmail.com>
2019-09-13 16:00:03 +0300

    norm

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fsinged <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/06 11:47:15 by fsinged           #+#    #+#             */
-/*   Updated: 2019/09/13 15:51:33 by fsinged          ###   ########.fr       */
+/*   Updated: 2019/09/13 15:59:35 by fsinged          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,47 +30,47 @@ typedef struct	s_ar
 ** Rules
 */
 
-void	swap(int *ar, int size, int h);
-void	swap_ab(t_ar *ar, int h);
-void	push(int *a, int *b, int *size, int *sizeb);
-void	push_ab(t_ar *ar, char c);
-void	rotate(int *ar, int size, int h);
-void	rotate_ab(t_ar *ar, int h);
-void	rrotate(int *ar, int size, int h);
-void	rrotate_ab(t_ar *ar, int h);
+void			swap(int *ar, int size, int h);
+void			swap_ab(t_ar *ar, int h);
+void			push(int *a, int *b, int *size, int *sizeb);
+void			push_ab(t_ar *ar, char c);
+void			rotate(int *ar, int size, int h);
+void			rotate_ab(t_ar *ar, int h);
+void			rrotate(int *ar, int size, int h);
+void			rrotate_ab(t_ar *ar, int h);
 
 /*
 ** functions.c
 */
 
-int		issorted(int *a, int size, char c);
-void	free_struct(t_ar **ar);
-t_ar	*init_ar(t_ar *ar, int size);
+int				issorted(int *a, int size, char c);
+void			free_struct(t_ar **ar);
+t_ar			*init_ar(t_ar *ar, int size);
 
-int		readnumbers(int argc, char **argv, t_ar *ar); 
-void	small_sort(int *a, int size);
-void	sort(t_ar *ar);
-void	big_sort(t_ar *ar, int avg);
+int				readnumbers(int argc, char **argv, t_ar *ar);
+void			small_sort(int *a, int size);
+void			sort(t_ar *ar);
+void			big_sort(t_ar *ar, int avg);
 
 /*
 ** help.c
 */
 
-int		get_avg(int *a, int size);
-int		get_max(int *b, int size);
-int		get_min(int *b, int size);
-int		*get_maxs(int *ar, int size, int *max);
-int		*get_mins(int *ar, int size, int *min);
+int				get_avg(int *a, int size);
+int				get_max(int *b, int size);
+int				get_min(int *b, int size);
+int				*get_maxs(int *ar, int size, int *max);
+int				*get_mins(int *ar, int size, int *min);
 
 /*
 ** pusher.c
 */
 
-void	push_b_third(t_ar *ar, int avg);
-void	push_a_second(t_ar *ar, int *min, int *cnt);
-void	push_b_first(t_ar *ar, int avg, int *min, int *cnt);
-void	push_a_first(t_ar *ar, int avg, int *min, int *cnt);
+void			push_b_third(t_ar *ar, int avg);
+void			push_a_second(t_ar *ar, int *min, int *cnt);
+void			push_b_first(t_ar *ar, int avg, int *min, int *cnt);
+void			push_a_first(t_ar *ar, int avg, int *min, int *cnt);
 
-void	print(int *a, int size);
+void			print(int *a, int size);
 
 #endif
